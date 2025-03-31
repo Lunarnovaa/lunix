89d7a7dfe7876aa6b6b650199515eb58ade0531d
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-03-30 17:47:27 -0700

    lib: cleanup listNixRecursive.nix

--- parts/lib/lunar/importers/listNixRecursive.nix
+++ parts/lib/lunar/importers/listNixRecursive.nix
@@ -1,27 +1,8 @@
 lib: let
   inherit (lib.trivial) pipe;
-  inherit (lib.strings) hasSuffix hasPrefix;
-  inherit (lib.lists) flatten;
-  inherit (lib.attrsets) mapAttrsToList filterAttrs;
-  inherit (builtins) filter readDir;
-
-  # taken from nixpkgs and modified by me so that any directories
-  # with the prefix _ would not have their files imported.
-  # yes, i decided to rewrite it as a pipe function.
-  # please note you can find the original function under
-  # lib.filesystem.listFilesRecursive
-  listFilesRecursiveClean = path:
-    pipe path [
-      readDir
-      (filterAttrs (n: _: (!hasPrefix "_" n))) # this is the one part i added to the function
-      (mapAttrsToList (
-        name: type:
-          if type == "directory"
-          then listFilesRecursiveClean (path + "/${name}")
-          else path + "/${name}"
-      ))
-      flatten
-    ];
+  inherit (lib.strings) hasSuffix;
+  inherit (lib.lunar.importers) listFilesRecursiveClean;
+  inherit (builtins) filter;
 in
   # This simple function just lists any nix files that are not the special
   # 'module.nix' file. Previously this was split into an "importNix" that
