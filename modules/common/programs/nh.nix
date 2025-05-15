5be4849cc59f50163f7b1a98d1e8068e3ef6fe4a
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-05-15 14:28:13 -0700

    service - system-maintenance: move update, cleanup, and optimise scripts into one

--- modules/common/programs/nh.nix
+++ modules/common/programs/nh.nix
@@ -1,8 +1,6 @@
 {...}: {
   programs.nh = {
     enable = true;
-    clean.enable = true;
-    clean.extraArgs = "--keep-since 7d --keep 5";
     flake = "/home/lunarnova/lunix";
   };
 }
--- modules/common/system/maintenance.nix
+++ modules/common/system/maintenance.nix
@@ -1,13 +1,10 @@
 {
   config,
-  lib,
   pkgs,
   ...
-}: let
-  inherit (lib.lists) singleton;
-in {
+}: {
   config = {
-    systemd.services.nh-update = {
+    systemd.services.system-maintenance = {
       description = "NixOS Update using NH";
 
       restartIfChanged = false;
@@ -20,28 +17,31 @@ in {
       path = with pkgs; [
         nh
         alacritty
+        nushell
       ];
 
       script = let
         alacritty = "${pkgs.alacritty}/bin/alacritty";
         nh = "${pkgs.nh}/bin/nh";
-      in ''
-        ${alacritty} -e ${nh} os boot ${config.programs.nh.flake} --update --ask
-      '';
+        nu = "${pkgs.nushell}/bin/nu";
+        nix-store = "${config.nix.package}/bin/nix-store";
+      in
+        # Couldn't figure out how to feed alacritty multiple programs so I just fed it into nu.
+        ''
+          ${alacritty} -e ${nu} -c "\
+            ${nh} os boot ${config.programs.nh.flake} --update --ask ; \
+            ${nh} clean all --keep-since 7d --keep 5 ; \
+            ${nix-store} --optimise"
+        '';
 
-      startAt = "daily";
+      startAt = "Mon,Wed,Sat 08:00:00";
 
       after = ["network-online.target"];
       wants = ["network-online.target"];
     };
 
-    systemd.timers.nh-update = {
+    systemd.timers.system-maintenance = {
       timerConfig.Persistent = true;
-    };
-
-    nix.optimise = {
-      automatic = true;
-      dates = singleton "08:00";
     };
   };
 }
