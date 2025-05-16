7f45b126b5e0809f9390a2aa107832e98a6a3f84
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-05-15 19:28:38 -0700

    keyd: init capslock layered arrow keys

---
+++ modules/common/programs/keyd.nix
@@ -0,0 +1,29 @@
+{...}: {
+  services.keyd = {
+    enable = true;
+    keyboards.default = {
+      ids = ["*"]; # Any keyboard
+
+      # For some reason, when using non-QWERTY keyboard layouts,
+      # keyd still accepts keys from their QWERTY position.
+      # e.g. to set `capslock-n = left`, one must set `capslock-j = left`
+      settings = {
+        main = {
+          capslock = "overload(capslock, esc)";
+        };
+        "capslock:C" = {
+          j = "left";
+          k = "down";
+          l = "right";
+          i = "up";
+        };
+        "capslock+shift" = {
+          j = "C-left";
+          k = "C-down";
+          l = "C-right";
+          i = "C-up";
+        };
+      };
+    };
+  };
+}
