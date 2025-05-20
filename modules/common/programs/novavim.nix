692f3c142d5259cdf85e0a7848330da015e965c7
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-05-19 18:50:09 -0700

    novavim: add EDITOR variables

--- modules/common/programs/novavim.nix
+++ modules/common/programs/novavim.nix
@@ -1,3 +1,5 @@
 {inputs', ...}: {
   environment.systemPackages = [inputs'.novavim.packages.default];
+  environment.variables.EDITOR = "nvim";
+  hjem.users.lunarnova.environment.sessionVariables.EDITOR = "nvim";
 }
