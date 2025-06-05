776e56b2837e2c4ea85fb84def30533c0b1f02ad
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-06-05 10:16:07 -0700

    git: init signers

--- modules/common/programs/git.nix
+++ modules/common/programs/git.nix
@@ -1,12 +1,32 @@
 {
-  programs.git = {
+  pkgs,
+  config,
+  ...
+}: let
+  signingKey = "${config.hjem.users.lunarnova.directory}/.ssh/id_ed25519.pub";
+  email = "35857948+Lunarnovaa@users.noreply.github.com";
+
+  signersFile = pkgs.writeText "git-allowed-signers" ''
+    ${email} namespaces="git" ${signingKey}
+  '';
+in {
+  config.hjem.users.lunarnova.files.".config/git/allowed_signers".source = signersFile;
+
+  config.programs.git = {
     enable = true;
     config = {
       user = {
-        email = "35857948+Lunarnovaa@users.noreply.github.com";
+        inherit email signingKey;
         name = "lunarnovaa";
       };
       init.defaultBranch = "main";
+
+      gpg = {
+        format = "ssh";
+        ssh.allowedSignersFile = signersFile;
+      };
+      commit.gpgSign = true;
+      tag.gpgSign = true;
     };
   };
 }
