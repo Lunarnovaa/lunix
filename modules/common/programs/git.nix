61018bf48b664bfe5a95f16e906072dca577cf60
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-06-06 12:03:36 -0700

    git: remove signing

--- modules/common/programs/git.nix
+++ modules/common/programs/git.nix
@@ -3,30 +3,33 @@
   config,
   ...
 }: let
-  signingKey = "${config.hjem.users.lunarnova.directory}/.ssh/polaris.pub";
   email = "35857948+Lunarnovaa@users.noreply.github.com";
+  /*
+    signingKey = "${config.hjem.users.lunarnova.directory}/.ssh/polaris.pub";
+  signingKey = "/etc/ssh/ssh_host_ed25519_key.pub";
 
   signersFile = pkgs.writeText "git-allowed-signers" ''
     ${email} namespaces="git" ${signingKey}
   '';
+  */
 in {
-  config.hjem.users.lunarnova.files.".config/git/allowed_signers".source = signersFile;
-
+  #config.hjem.users.lunarnova.files.".config/git/allowed_signers".source = signersFile;
   config.programs.git = {
     enable = true;
     config = {
       user = {
-        inherit email signingKey;
+        inherit email; #signingKey;
         name = "lunarnovaa";
       };
       init.defaultBranch = "main";
-
+      /*
       gpg = {
         format = "ssh";
         ssh.allowedSignersFile = signersFile;
       };
       commit.gpgSign = true;
       tag.gpgSign = true;
+      */
     };
   };
 }
