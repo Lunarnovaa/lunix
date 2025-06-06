0675879ac21037fe4243869a8a8d8edbb99fc18f
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-06-06 12:47:46 -0700

    docs: filter non-lunix options

--- parts/packages/pkgs/docs.nix
+++ parts/packages/pkgs/docs.nix
@@ -8,7 +8,7 @@
   lunixpkgs,
 }: let
   inherit (builtins) isAttrs toString;
-  inherit (lib.attrsets) isDerivation mapAttrs optionalAttrs;
+  inherit (lib.attrsets) isDerivation mapAttrs optionalAttrs filterAttrs;
   inherit (lib.modules) mkForce evalModules;
   inherit (lib.options) mkOption;
   inherit (lib.strings) hasPrefix removePrefix;
@@ -23,62 +23,61 @@
       warningsAreErrors = false;
 
       # Feed the generator the options
-      inherit
-        (
-          (evalModules {
-            specialArgs = {inherit lib inputs self' inputs' theme lunixpkgs;};
-            # There's no reason to import hosts, since it neither declares options nor sets necessary config
-            modules =
-              (listNixRecursive moduleDir)
-              ++ [
-                (let
-                  # From nixpkgs:
-                  #
-                  # Recursively replace each derivation in the given attribute set
-                  # with the same derivation but with the `outPath` attribute set to
-                  # the string `"\${pkgs.attribute.path}"`. This allows the
-                  # documentation to refer to derivations through their values without
-                  # establishing an actual dependency on the derivation output.
-                  #
-                  # This is not perfect, but it seems to cover a vast majority of use cases.
-                  #
-                  # Caveat: even if the package is reached by a different means, the
-                  # path above will be shown and not e.g.
-                  # `${config.services.foo.package}`.
-                  scrubDerivations = namePrefix: pkgSet:
-                    mapAttrs (
-                      name: value: let
-                        wholeName = "${namePrefix}.${name}";
-                      in
-                        if isAttrs value
-                        then
-                          scrubDerivations wholeName value
-                          // optionalAttrs (isDerivation value) {
-                            inherit (value) drvPath;
-                            outPath = "\${${wholeName}}";
-                          }
-                        else value
-                    )
-                    pkgSet;
-                in {
-                  _module = {
-                    check = false;
-                    args.pkgs = mkForce (scrubDerivations "pkgs" pkgs);
+      options =
+        filterAttrs (n: _: n == "lunix")
+        (evalModules {
+          specialArgs = {inherit lib inputs self' inputs' theme lunixpkgs;};
+          # There's no reason to import hosts, since it neither declares options nor sets necessary config
+          modules =
+            (listNixRecursive moduleDir)
+            ++ [
+              (let
+                # From nixpkgs:
+                #
+                # Recursively replace each derivation in the given attribute set
+                # with the same derivation but with the `outPath` attribute set to
+                # the string `"\${pkgs.attribute.path}"`. This allows the
+                # documentation to refer to derivations through their values without
+                # establishing an actual dependency on the derivation output.
+                #
+                # This is not perfect, but it seems to cover a vast majority of use cases.
+                #
+                # Caveat: even if the package is reached by a different means, the
+                # path above will be shown and not e.g.
+                # `${config.services.foo.package}`.
+                scrubDerivations = namePrefix: pkgSet:
+                  mapAttrs (
+                    name: value: let
+                      wholeName = "${namePrefix}.${name}";
+                    in
+                      if isAttrs value
+                      then
+                        scrubDerivations wholeName value
+                        // optionalAttrs (isDerivation value) {
+                          inherit (value) drvPath;
+                          outPath = "\${${wholeName}}";
+                        }
+                      else value
+                  )
+                  pkgSet;
+              in {
+                _module = {
+                  check = false;
+                  args.pkgs = mkForce (scrubDerivations "pkgs" pkgs);
+                };
+              })
+              # avoid having `_module.args` in the documentation
+              {
+                options = {
+                  _module.args = mkOption {
+                    internal = true;
                   };
-                })
-                # avoid having `_module.args` in the documentation
-                {
-                  options = {
-                    _module.args = mkOption {
-                      internal = true;
-                    };
-                  };
-                }
-              ];
-          })
-        )
-        options
-        ;
+                };
+              }
+            ];
+        })
+        .
+        options;
       transformOptions = opt:
         opt
         // {
