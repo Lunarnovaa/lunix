4f75e8b02f9cde1ef7b8ec8f878fad30d904100a
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-06-13 08:52:19 -0700

    general secrets and networking cleanup

--- modules/common/system/networking.nix
+++ modules/common/system/networking.nix
@@ -1,6 +1,4 @@
-{config, ...}: let
-  inherit (config.age.secrets) wifiPassword;
-in {
+{
   # Enable Networking
   networking = {
     networkmanager = {
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,9 +3,8 @@ let
   #sourced from /etc/ssh/ssh_host_ed25519_key.pub
   polaris = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG2iAb15Y/Zwrt8k5BPfneZ0Cl8tcr7bEPZ4/UUlCZIG";
   procyon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAN3HCuUY6vW6TugIHSvoUjJP31kZfomDV63KZTuSlnO";
+  publicKeys = [polaris procyon];
 in {
-  "userPassword.age".publicKeys = [polaris procyon];
-  "wifiPassword.age".publicKeys = [polaris procyon];
-  "spotifyPassword.age".publicKeys = [polaris procyon];
-  "spotifyClientID.age".publicKeys = [polaris procyon];
+  "spotifyPassword.age" = {inherit publicKeys;};
+  "spotifyClientID.age" = {inherit publicKeys;};
 }
