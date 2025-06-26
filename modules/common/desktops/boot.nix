5251576ab75488e851aa21957a7c294f90464734
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-06-26 09:39:35 -0700

    console: use colemak and base16 colors

---
+++ modules/common/desktops/boot.nix
@@ -0,0 +1,38 @@
+{
+  theme,
+  pkgs,
+  ...
+}: let
+  inherit (theme.fonts) monospace;
+  inherit (theme.colors) base00 base01 base02 base03 base04 base05 base06 base07 base08 base09 base10 base11 base12 base13 base14 base15;
+in {
+  boot = {
+    plymouth = {
+      enable = true;
+      themePackages = [pkgs.plymouth-blahaj-theme];
+      theme = "blahaj";
+      font = monospace.file;
+    };
+    kernelParams = ["quiet"];
+  };
+  console = {
+    colors = [
+      base00
+      base01
+      base02
+      base03
+      base04
+      base05
+      base06
+      base07
+      base08
+      base09
+      base10
+      base11
+      base12
+      base13
+      base14
+      base15
+    ];
+  };
+}
--- modules/common/desktops/keyboard.nix
+++ modules/common/desktops/keyboard.nix
@@ -4,4 +4,6 @@
     layout = "us";
     variant = "colemak";
   };
+  # Enable colemak in the tty + boot console
+  console.useXkbConfig = true;
 }
