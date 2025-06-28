185348d04a4ea9ddc2cd9aab0b66a7f536069ef4
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-06-27 20:24:10 -0700

    shell: remove novavim launch prompt

--- parts/shell.nix
+++ parts/shell.nix
@@ -1,33 +1,24 @@
-{lib, ...}: {
+{
   perSystem = {
     pkgs,
     inputs',
     config,
     ...
-  }: let
-    inherit (lib.meta) getExe;
-  in {
+  }: {
     # More minimal shell environment, excluding C Compilers
     devShells.default = pkgs.mkShellNoCC {
       name = "lunix";
 
       shellHook = let
-        green = ''\033[1;32m'';
+        #green = ''\033[1;32m'';
         cyan = ''\033[1;36m'';
-        red = ''\033[1;31m'';
+        #red = ''\033[1;31m'';
         noColor = ''\033[0m'';
       in
         # Bash
         ''
           ${config.pre-commit.installationScript}
-          echo -e "${cyan}Welcome to Lunix."
-
-          echo -e "Launch Novavim? [${green}y/${red}N${cyan}]${noColor}"
-          read -n 1 launch
-
-          if [ "$launch" = "y" ]; then
-            ${getExe inputs'.novavim.packages.lunix}
-          fi
+          echo -e "${cyan}Welcome to Lunix.${noColor}"
         '';
 
       packages = [
