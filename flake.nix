bd27bad0a761832ccdd65804b00cbfd329673620
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-07-06 13:39:31 -0700

    flake: cleanup inputs

--- flake.nix
+++ flake.nix
@@ -15,32 +15,6 @@
     };
 
   inputs = {
-    ## Systems is a fancy flake to unify the systems ##
-    ## for which the flake and its inputs are built. ##
-
-    systems = {
-      # Currently only use x86_64-linux :)
-      url = "github:nix-systems/x86_64-linux";
-    };
-
-    # use the unstable branch
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
-    # for docs
-    ndg = {
-      url = "github:lunarnovaa/ndg?ref=improve-toc";
-      inputs = {
-        flake-compat.follows = "flake-compat";
-        flake-parts.follows = "flake-parts";
-      };
-    };
-    ## system infrastructure ##
-
-    # used for my laptop
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master"; #no nixpkgs necessary
-
-    impermanence.url = "github:nix-community/impermanence";
-
     # secrets management
     agenix = {
       url = "github:ryantm/agenix";
@@ -53,34 +27,13 @@
       };
     };
 
-    # modularizing my flake
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-    };
-
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # managing pre-commit hooks with nix
-    git-hooks = {
-      url = "github:cachix/git-hooks.nix";
+    # base16 palettes in nix
+    basix = {
+      url = "github:notashelf/basix";
       inputs = {
         nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
         flake-compat.follows = "flake-compat";
-      };
-    };
-
-    ## module specific stuff ##
-
-    # manage wi-fi through launcher
-    iwmenu = {
-      url = "github:e-tho/iwmenu";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-        rust-overlay.follows = "rust-overlay";
       };
     };
 
@@ -94,12 +47,63 @@
       };
     };
 
-    # a spotify ricer
-    spicetify-nix = {
-      url = "github:Gerg-L/spicetify-nix";
+    # modularizing my flake
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+    };
+
+    # managing pre-commit hooks with nix
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        systems.follows = "systems";
+        flake-compat.follows = "flake-compat";
+      };
+    };
+
+    # hjem, a replacement for home-manager's tooling
+    hjem = {
+      url = "github:feel-co/hjem";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hjem-rum = {
+      url = "github:snugnug/hjem-rum/";
+      #url = "github:nezia1/hjem-rum?ref=use-formats";
+      #url = "path:/home/lunarnova/projects/snug/hjem-rum";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        hjem.follows = "hjem";
+        ndg.follows = "ndg";
+        treefmt-nix.follows = "treefmt-nix";
+      };
+    };
+
+    impermanence.url = "github:nix-community/impermanence";
+
+    # manage wi-fi through launcher
+    iwmenu = {
+      url = "github:e-tho/iwmenu";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+        rust-overlay.follows = "rust-overlay";
+      };
+    };
+
+    # Lunar's (Nix) Libraries
+    lunarsLib = {
+      url = "github:lunarnovaa/lunarslib";
+      #url = "path:/home/lunarnova/projects/LunarsLibraries";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # for docs
+    ndg = {
+      url = "github:lunarnovaa/ndg?ref=improve-toc";
+      inputs = {
+        flake-compat.follows = "flake-compat";
+        flake-parts.follows = "flake-parts";
       };
     };
 
@@ -113,17 +117,11 @@
       };
     };
 
-    ## shelfware ##
+    # used for my laptop
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master"; #no nixpkgs necessary
 
-    # base16 palettes in nix
-    basix = {
-      url = "github:notashelf/basix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-parts.follows = "flake-parts";
-        flake-compat.follows = "flake-compat";
-      };
-    };
+    # use the unstable branch
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
     # Lunarnova's Neovim Configuration
     novavim = {
@@ -136,55 +134,31 @@
       };
     };
 
-    # Lunar's (Nix) Libraries
-    lunarsLib = {
-      url = "github:lunarnovaa/lunarslib";
-      #url = "path:/home/lunarnova/projects/LunarsLibraries";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    /*
-    schizofox = {
-      url = "path:/home/lunarnova/schizofox";
+    # a spotify ricer
+    spicetify-nix = {
+      url = "github:Gerg-L/spicetify-nix";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        hjem.follows = "hjem";
-        home-manager.follows = "";
-        flake-parts.follows = "flake-parts";
         systems.follows = "systems";
-        flake-compat.follows = "flake-compat";
       };
     };
-    */
-    ## hjem business ##
 
-    # hjem, a replacement for home-manager's tooling
-    hjem = {
-      url = "github:feel-co/hjem";
+    # Currently only use x86_64-linux :)
+    systems.url = "github:nix-systems/x86_64-linux";
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # i use a local version for dogfeeding
-    hjem-rum = {
-      url = "github:snugnug/hjem-rum/";
-      #url = "github:nezia1/hjem-rum/use-formats";
-      #url = "path:/home/lunarnova/projects/snug/hjem-rum";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        hjem.follows = "hjem";
-        ndg.follows = "ndg";
-        treefmt-nix.follows = "treefmt-nix";
-      };
     };
 
     # input unification - not used here, but so that we can decrease redundant flake inputs
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+    };
+
     flake-utils = {
       url = "github:numtide/flake-utils";
       inputs.systems.follows = "systems";
-    };
-
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
     };
 
     nvf = {
