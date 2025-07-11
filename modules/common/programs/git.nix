4667b0fb8c889ac02d58a2c0b00d481124f31226
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-07-11 11:52:50 -0700

    git: add signing

--- modules/common/programs/git.nix
+++ modules/common/programs/git.nix
@@ -3,33 +3,37 @@
   config,
   ...
 }: let
-  email = "35857948+Lunarnovaa@users.noreply.github.com";
-  /*
-    signingKey = "${config.hjem.users.lunarnova.directory}/.ssh/polaris.pub";
-  signingKey = "/etc/ssh/ssh_host_ed25519_key.pub";
+  yaml = pkgs.formats.yaml {};
 
+  email = "35857948+Lunarnovaa@users.noreply.github.com";
+  signingKey = "${config.hjem.users.lunarnova.directory}/.ssh/id_ed25519.pub";
   signersFile = pkgs.writeText "git-allowed-signers" ''
     ${email} namespaces="git" ${signingKey}
   '';
-  */
 in {
-  #config.hjem.users.lunarnova.files.".config/git/allowed_signers".source = signersFile;
-  config.programs.git = {
-    enable = true;
-    config = {
-      user = {
-        inherit email; #signingKey;
-        name = "lunarnovaa";
+  config = {
+    hjem.users.lunarnova.files = {
+      ".config/git/allowed_signers".source = signersFile;
+      ".config/lazygit/config.yml".source = yaml.generate "lazygit-config.yml" {
+        git.overrideGpg = true;
       };
-      init.defaultBranch = "main";
-      /*
-      gpg = {
-        format = "ssh";
-        ssh.allowedSignersFile = signersFile;
+    };
+    programs.git = {
+      enable = true;
+      config = {
+        user = {
+          inherit email signingKey;
+          name = "lunarnovaa";
+        };
+        init.defaultBranch = "main";
+
+        gpg = {
+          format = "ssh";
+          ssh.allowedSignersFile = signersFile;
+        };
+        commit.gpgSign = true;
+        tag.gpgSign = true;
       };
-      commit.gpgSign = true;
-      tag.gpgSign = true;
-      */
     };
   };
 }
