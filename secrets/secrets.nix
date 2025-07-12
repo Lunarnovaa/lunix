2fe9f9c8dc0eb492da8913438cbaabfabcfa869e
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-07-12 14:44:34 -0700

    polaris: update host key for agenix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
-  #generated with sudo ssh-keygen -A
-  #sourced from /etc/ssh/ssh_host_ed25519_key.pub
-  polaris = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG2iAb15Y/Zwrt8k5BPfneZ0Cl8tcr7bEPZ4/UUlCZIG";
+  # generated with sudo ssh-keygen -A
+  # sourced from /etc/ssh/ssh_host_ed25519_key.pub
+  # with impermanence, that's under /persist/etc/ssh
+  polaris = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOeTOmn3rRyNnN6N7RToQRFuETFgTmkb2G85PeuFqXYd";
   procyon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIGYSp7arWyY+hebvg67Ze1fe1s4AvneR9A5VvOFHpuw";
   publicKeys = [polaris procyon];
 in {
