48d134cb733da0e4536f7871db6089991d334d9d
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-07-12 14:31:48 -0700

    procyon: new ssh key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   #generated with sudo ssh-keygen -A
   #sourced from /etc/ssh/ssh_host_ed25519_key.pub
   polaris = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG2iAb15Y/Zwrt8k5BPfneZ0Cl8tcr7bEPZ4/UUlCZIG";
-  procyon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAN3HCuUY6vW6TugIHSvoUjJP31kZfomDV63KZTuSlnO";
+  procyon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIGYSp7arWyY+hebvg67Ze1fe1s4AvneR9A5VvOFHpuw";
   publicKeys = [polaris procyon];
 in {
   "spotifyPassword.age" = {inherit publicKeys;};
