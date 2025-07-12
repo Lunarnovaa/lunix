e85a776878255ec6b2b85236e58deabc47c9fc63
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-07-12 14:29:11 -0700

    agenix: minor refactor

--- modules/common/users/agenix.nix
+++ modules/common/users/agenix.nix
@@ -11,14 +11,18 @@
 
   cfgImpermanence = config.lunix.hardware.impermanence;
 in {
-  environment.systemPackages = [inputs'.agenix.packages.default];
-  age.secrets = {
-    spotifyPassword.file = secretsDir + /spotifyPassword.age;
-    spotifyClientID.file = secretsDir + /spotifyClientID.age;
-  };
   imports = [inputs.agenix.nixosModules.default];
-  age.identityPaths = [
-    "${optionalString cfgImpermanence.enable "/persist"}/etc/ssh/ssh_host_ed25519_key"
-    "${optionalString cfgImpermanence.enable "/persist"}/etc/ssh/ssh_host_rsa_key"
-  ];
+  config = {
+    environment.systemPackages = [inputs'.agenix.packages.default];
+    age = {
+      secrets = {
+        spotifyPassword.file = secretsDir + /spotifyPassword.age;
+        spotifyClientID.file = secretsDir + /spotifyClientID.age;
+      };
+      identityPaths = [
+        "${optionalString cfgImpermanence.enable "/persist"}/etc/ssh/ssh_host_ed25519_key"
+        "${optionalString cfgImpermanence.enable "/persist"}/etc/ssh/ssh_host_rsa_key"
+      ];
+    };
+  };
 }
