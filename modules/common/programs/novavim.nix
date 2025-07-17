5dbfc9cb75b366e1c50dcb0c6845a42320871d6a
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-07-16 17:47:08 -0700

    set helix to $EDITOR
    also adds kdl formatter i think

--- modules/common/programs/novavim.nix
+++ modules/common/programs/novavim.nix
@@ -1,5 +1,3 @@
 {inputs', ...}: {
   environment.systemPackages = [inputs'.novavim.packages.default];
-  environment.variables.EDITOR = "nvim";
-  hjem.users.lunarnova.environment.sessionVariables.EDITOR = "nvim";
 }
