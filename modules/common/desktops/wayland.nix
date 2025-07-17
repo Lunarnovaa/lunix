445526e7ba2e709fb1a96971a6623aa31b4075c5
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-07-16 17:46:53 -0700

    wayland: add wl-clipboard

--- modules/common/desktops/wayland.nix
+++ modules/common/desktops/wayland.nix
@@ -1,9 +1,12 @@
-{config, ...}: let
+{pkgs, ...}: let
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 in {
   # Make Chromium + Electron apps use Wayland
   config = {
     inherit environment;
-    hjem.users.lunarnova = {inherit environment;};
+    hjem.users.lunarnova = {
+      inherit environment;
+      packages = [pkgs.wl-clipboard-rs];
+    };
   };
 }
