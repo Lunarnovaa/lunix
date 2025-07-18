ca27592defb81fcb38cc6fd8ec67cda714ddd1bf
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-07-18 15:39:09 -0700

    programs/terminal: fix default aliases

--- modules/common/programs/terminal/terminal.nix
+++ modules/common/programs/terminal/terminal.nix
@@ -10,15 +10,16 @@ in {
     lunix.terminal = {
       aliases = mkOption {
         type = attrsOf str;
-        default = {
-          ll = "${pkgs.eza}/bin/eza -l";
-          lt = "${pkgs.eza}/bin/eza --tree";
-          ndev = "nix develop";
-          nrun = "nix run";
-          spp = "spotify_player";
-        };
+        default = {};
         description = "Aliases to be added to your user shell.";
       };
     };
+  };
+  config.lunix.terminal.aliases = {
+    ll = "${pkgs.eza}/bin/eza -l";
+    lt = "${pkgs.eza}/bin/eza --tree";
+    ndev = "nix develop";
+    nrun = "nix run";
+    spp = "spotify_player";
   };
 }
