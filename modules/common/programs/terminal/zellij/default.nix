699b1dd1d5cec04aa0b7c4bc8e6f173d68b4678f
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-07-19 15:52:58 -0700

    programs/terminal: add zj alias + rework zj keybinds

--- modules/common/programs/terminal/zellij/default.nix
+++ modules/common/programs/terminal/zellij/default.nix
@@ -1,4 +1,5 @@
 {pkgs, ...}: {
+  lunix.programs.terminal.aliases.zj = "zellij";
   hjem.users.lunarnova = {
     packages = [pkgs.zellij];
     files.".config/zellij/config.kdl".source = pkgs.concatText "zellij-config" [
