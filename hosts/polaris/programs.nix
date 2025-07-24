798dce70eabdf1b8730ac5bb3ceaeec3d2db3930
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-07-24 12:23:18 -0700

    obs: fix plugins; polaris: enable obs

--- hosts/polaris/programs.nix
+++ hosts/polaris/programs.nix
@@ -4,6 +4,7 @@
       enable = true;
       app = "mozilla";
     };
+    obs.enable = true;
     spicetify.enable = true;
     vial.enable = false;
   };
--- modules/profiles/gaming/programs/obs.nix
+++ modules/profiles/gaming/programs/obs.nix
@@ -26,15 +26,17 @@ in {
     hjem.users.lunarnova = {
       rum.programs.obs-studio = {
         enable = true;
-        plugins = with pkgs.obs-studio-plugins; [
-          wlrobs #for screen capture w/wayland, will be mandatory in future
-          obs-vkcapture #vulkan/opengl game capture #ref https://github.com/nowrep/obs-vkcapture for game setup
-          input-overlay #kb, contr, & mouse input
-          obs-multi-rtmp #multi-site broadcast
-          obs-mute-filter #improves muting capability for sources
-          obs-livesplit-one #livesplit one source
-          obs-pipewire-audio-capture
-        ];
+        package = pkgs.wrapOBS {
+          plugins = with pkgs.obs-studio-plugins; [
+            wlrobs #for screen capture w/wayland, will be mandatory in future
+            obs-vkcapture #vulkan/opengl game capture #ref https://github.com/nowrep/obs-vkcapture for game setup
+            input-overlay #kb, contr, & mouse input
+            obs-multi-rtmp #multi-site broadcast
+            obs-mute-filter #improves muting capability for sources
+            obs-livesplit-one #livesplit one source
+            obs-pipewire-audio-capture
+          ];
+        };
       };
       xdg.config.files."obs-studio/themes".source = "${catppuccin-mocha}/themes";
     };
