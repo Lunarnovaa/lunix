ec90e5e6a17307e1902d681543f2eaacc804b7a0
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-07-30 11:53:40 -0700

    desktops/niri: use environmental variables

--- modules/desktops/niri/module.nix
+++ modules/desktops/niri/module.nix
@@ -5,9 +5,18 @@
   config,
   ...
 }: let
+  inherit (builtins) concatStringsSep;
+  inherit (lib.attrsets) mapAttrsToList;
   inherit (lib.filesystem) listFilesRecursive;
+  inherit (lib.lists) singleton;
   inherit (lib.modules) mkIf mkDefault;
   inherit (lib.options) mkEnableOption;
+  inherit (lib.trivial) pipe;
+
+  niriEnvironment = pipe config.hjem.users.lunarnova.environment.sessionVariables [
+    (mapAttrsToList (n: v: n + " \"${v}\""))
+    (concatStringsSep "\n")
+  ];
 
   cfg = config.lunix.desktops.niri;
 in {
@@ -33,7 +42,15 @@ in {
         pkgs.xwayland-satellite
         pkgs.brightnessctl
       ];
-      xdg.config.files."niri/config.kdl".source = pkgs.concatText "niri-config" (listFilesRecursive ./config);
+      xdg.config.files."niri/config.kdl".source = pkgs.concatText "niri-config" (
+        listFilesRecursive ./config
+        ++ singleton (pkgs.writeText "niri-env-vars" ''
+          environment {
+            ${niriEnvironment}
+            DISPLAY ":0"
+          }
+        '')
+      );
     };
   };
 }
