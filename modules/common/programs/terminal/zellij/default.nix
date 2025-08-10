6ae703fa44b6654712a328747b4d4e95c1e6175a
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-08-10 11:50:25 -0700

    programs/zellij: add edit alias

--- modules/common/programs/terminal/zellij/default.nix
+++ modules/common/programs/terminal/zellij/default.nix
@@ -1,5 +1,14 @@
 {pkgs, ...}: {
-  lunix.programs.terminal.aliases.zj = "zellij";
+  lunix.programs.terminal.aliases = {
+    zj = "zellij";
+    edit =
+      "zellij --layout "
+      + pkgs.writeText "standard-layout.kdl" ''
+        layout {
+          pane command="hx"
+        }
+      '';
+  };
   hjem.users.lunarnova = {
     packages = [pkgs.zellij];
     xdg.config.files."zellij/config.kdl".source = pkgs.concatText "zellij-config" [
