b002a0ee0e0ac530264fa713957ce4e4d87238e6
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2026-06-22 12:26:04 -0700

    bootloader: switch to limine

--- hosts/polaris/configuration.nix
+++ hosts/polaris/configuration.nix
@@ -3,6 +3,7 @@
     profiles.gaming.enable = true;
     hardware = {
       nvidia.enable = true;
+      display.resolution = "2560x1440";
     };
     programs = {
       firefox = {
--- modules/system/bootloader.nix
+++ modules/system/bootloader.nix
@@ -1,11 +1,52 @@
 {
-  # Configure the Bootloader
-  boot.loader = {
-    systemd-boot = {
-      enable = true;
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib.options) mkOption;
+  inherit (lib.types) str;
+
+  cfg = config.lunix.hardware.display;
+in {
+  options = {
+    lunix.hardware.display = {
+      resolution = mkOption {
+        type = str;
+        default = "1920x1080";
+        example = "2560x1440";
+        description = "The resolution of the display.";
+      };
     };
-    efi = {
-      canTouchEfiVariables = true;
+  };
+
+  # Configure the Bootloader
+  config = {
+    boot.loader = {
+      limine = {
+        enable = true;
+        style = {
+          wallpapers = [../../assets/catppuccin-hollow-knight.jpg];
+          interface = {
+            inherit (cfg) resolution;
+            branding = "Lunix: Booting into ${config.networking.hostName}.";
+          };
+        };
+        extraConfig = ''
+          # catppuccin-mocha-pink from catppuccin/limine
+          term_palette: 1e1e2e;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4
+          term_palette_bright: 585b70;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4
+          term_background: 091e1e2e # 0.9 transparency
+          term_foreground: cdd6f4
+          term_background_bright: 09585b70
+          term_foreground_bright: cdd6f4
+          interface_branding_color: f5c2e7
+          interface_help_color: f5c2e7
+          interface_help_color_bright: f5c2e7
+        '';
+      };
+      efi = {
+        canTouchEfiVariables = true;
+      };
     };
   };
 }
