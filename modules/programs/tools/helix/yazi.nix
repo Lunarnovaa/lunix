655f96eeb8b8d610259ace484add3e479e28fc3b
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2026-06-22 16:51:11 -0700

    yazi: init

---
+++ modules/programs/tools/helix/yazi.nix
@@ -0,0 +1,27 @@
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  environment.shellAliases.y = "yazi";
+  programs.yazi = {
+    enable = true;
+    flavors.catppuccin-mocha = "${inputs.yazi-flavors}/catppuccin-mocha.yazi";
+    plugins = {
+      inherit (pkgs.yaziPlugins) rich-preview smart-enter;
+    };
+    settings = {
+      theme.flavor.dark = "catppuccin-mocha";
+      #yazi = {};
+      keymap = {
+        mgr.prepend_keymap = [
+          {
+            on = "<Right>";
+            run = "plugin smart-enter";
+            desc = "Enter the child directory, or open the file";
+          }
+        ];
+      };
+    };
+  };
+}
