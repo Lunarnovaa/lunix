14cce9df4768be0b0326f4ac526f89be93130f11
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2026-06-28 10:19:59 -0700

    remove wl-clipboard-rs
    honestly idk why this was there. i think niri. hopefully this wasn't
    keeping anything together lmfao

--- modules/graphical/wayland.nix
+++ modules/graphical/wayland.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
   # Make Chromium + Electron apps use Wayland
-  hjem.users.lunarnova.packages = [pkgs.wl-clipboard-rs];
+  # hjem.users.lunarnova.packages = [pkgs.wl-clipboard-rs];
   lunix.environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
