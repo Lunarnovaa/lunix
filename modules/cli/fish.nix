ac27356e33284fdce0585b4fe8aed8317537f382
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2026-07-09 13:56:34 -0700

    fish: fix theme file

--- modules/cli/fish.nix
+++ modules/cli/fish.nix
@@ -8,7 +8,7 @@
   inherit (lib.meta) getExe;
 in {
   users.extraUsers.lunarnova.shell = pkgs.fish;
-  hjem.users.lunarnova.xdg.config.files."fish/themes/catppuccin-mocha.theme".source = "${inputs.catppuccin-fish}/static/catppuccin-mocha.theme";
+  hjem.users.lunarnova.xdg.config.files."fish/themes/catppuccin-mocha.theme".source = "${inputs.catppuccin-fish}/themes/static/catppuccin-mocha.theme";
   programs.fish = {
     enable = true;
     inherit (config.environment) shellAliases;
