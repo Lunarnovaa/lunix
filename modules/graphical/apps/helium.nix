95bdbab9505b653c3c57aef3f0aab7046b52aa8e
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2026-07-12 21:39:44 -0700

    helium: make default browser

--- modules/graphical/apps/helium.nix
+++ modules/graphical/apps/helium.nix
@@ -17,5 +17,13 @@ in {
 
   config = mkIf cfg.enable {
     environment.systemPackages = [inputs.helium.packages."x86_64-linux".default];
+    lunix.environment.sessionVariables.DEFAULT_BROWSER = "helium";
+    hjem.users.lunarnova.xdg.mime-apps.default-applications = {
+      "text/html" = ["helium"];
+      "x-scheme-handler/http" = ["helium"];
+      "x-scheme-handler/https" = ["helium"];
+      "x-scheme-handler/about" = ["helium"];
+      "x-scheme-handler/unknown" = ["helium"];
+    };
   };
 }
